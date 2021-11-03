14a4d91030f88580c8470a372491a5d2ce0f119e
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-04 08:48:55 +0900

    on/offで値を切り替える

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickFaceSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickCountUp}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickFaceSwichShowFlag}>on/off</button>
       <p>{num}</p>
+      {setFaceShowFlag && <p>♪(´ε｀ )</p>}
     </>
   );
 };
